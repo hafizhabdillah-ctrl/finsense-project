d588107836976f3916a2081807dcd63193e48058
Hafizh Kusuma Abdillah <hafizh.abdillah@mhs.unsoed.ac.id>
2026-05-10 15:56:23 +0700

    added AI support chat template

--- src/Components/Layout/MainLayout.jsx
+++ src/Components/Layout/MainLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import TopBar from './Topbar';
+import Chat from '../Features/Chat/Chat';
 
 function MainLayout() {
   return (
@@ -10,14 +11,17 @@ function MainLayout() {
       {/* Sidebar */}
       <Sidebar />
 
-      {/* TopBar + Content */}
+      {/* TopBar */}
       <div className="flex-1 flex flex-col">
         <TopBar />
 
+        {/* Content */}
         <div className="flex-1">
           <Outlet />
         </div>
       </div>
+
+      <Chat />
     </div>
   );
 }
