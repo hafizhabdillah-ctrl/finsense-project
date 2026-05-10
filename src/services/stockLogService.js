9cecc3798889777873ad532a98fdc7f15d4ec9f0
rifkiardiansahh <rifkiardiansah96@gmail.com>
2026-05-10 17:54:08 +0700

    [Frontend] Fix LoginPage & Strukturalisasi Folder

--- src/services/stockLogService.js
+++
@@ -1,4 +0,0 @@
-import api from './api';
-
-export const getStockLogs = (params) => api.get('/stock-logs', { params });
-export const createStockLog = (data) => api.post('/stock-logs', data);
--- src/utils/local/chat.js
+++
@@ -1,7 +0,0 @@
-export const initialMessages = [
-  {
-    id: 1,
-    sender: 'ai',
-    text: 'Halo! Ada yang bisa saya bantu terkait fitur FinSense hari ini?'
-  }
-];
