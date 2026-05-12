9e0e9d315700d3cba190bfff4c319a82e3143141
rifkiardiansahh <rifkiardiansah96@gmail.com>
2026-05-13 01:29:19 +0700

    [Backend] fix cors (app.js)

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,6 @@ const app = express();
 app.use(
   cors({
     origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   }),
