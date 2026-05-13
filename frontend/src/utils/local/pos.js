a0ae15c966d5a07c509e28d7a90a7c16aabdd480
Hafizh Kusuma Abdillah <hafizh.abdillah@mhs.unsoed.ac.id>
2026-05-13 17:04:12 +0700

    [Frontend] Deleted dummy datas

--- frontend/src/utils/local/pos.js
+++ frontend/src/utils/local/pos.js
@@ -1,13 +1,4 @@
-export let carts = [
-  { id:1, name: 'Beras Premium', price: 10000, qty: 2 },
-  { id:2, name: 'Minyak Goreng', price: 20000, qty: 1 },
-  { id:3, name: 'Telur 1kg', price: 15000, qty: 3 },
-  { id:4, name: 'Ayam 1kg', price: 35000, qty: 1 },
-  { id:5, name: 'Beras Premium', price: 10000, qty: 2 },
-  { id:6, name: 'Minyak Goreng', price: 20000, qty: 1 },
-  { id:7, name: 'Telur 1kg', price: 15000, qty: 3 },
-  { id:8, name: 'Ayam 1kg', price: 35000, qty: 1 },
-];
+export let carts = [];
 
 export function addCart({ name, price, qty }) {
   carts = [...carts, {
