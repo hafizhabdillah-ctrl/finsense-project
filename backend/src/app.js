70bfd3228be7511ef970d7a233330cb258ea161a
rifkiardiansahh <rifkiardiansah96@gmail.com>
2026-05-27 21:58:08 +0700

    bug fixing

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
-      'https://finsense-project.vercel.app/',
+      'https://finsense-project.vercel.app',
     ],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
