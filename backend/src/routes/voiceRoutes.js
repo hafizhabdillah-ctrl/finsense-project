b4b3260a787f2c5489a3f5398270737b8070181e
rifkiardiansahh <rifkiardiansah96@gmail.com>
2026-06-03 04:08:54 +0700

    Fix: Voice Recognition

--- backend/src/routes/voiceRoutes.js
+++ backend/src/routes/voiceRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+// const auth = require('../middleware/auth');
 const router = express.Router();
 const voiceController = require('../controllers/voiceController');
+
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
 router.post('/voice', upload.single('audio'), voiceController.processVoice);
-router.post('/voice-transcript', voiceController.processTranscript);
 
 module.exports = router;
